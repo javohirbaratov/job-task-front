7141d8e2105debfdf03acb7f25630ce40e4738fb
Javohir Baratov <91523188+javohirbaratov@users.noreply.github.com>
2024-06-05 09:50:55 +0500

    "Refactor Documents page to use MaterialReactTable and add document preview button"

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,6 +1,6 @@
 // Private
 export const main_routes = {
   documents: "/",
-  documentsShow: "/document/:id/show",
-  documentsCreate: "/document/create",
+  documentShow: "/document/:id/show",
+  documentCreate: "/document/create",
 }
--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,22 +1,86 @@
-import { useMemo } from "react";
-import { useGetDocumentQuery } from "../app/services/document";
+import { useCallback, useMemo } from "react";
+import { IDocument, useGetDocumentQuery } from '../app/services/document';
+import { MaterialReactTable, MRT_ColumnDef, useMaterialReactTable } from "material-react-table";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { main_routes } from "../constants/path";
+
+interface IDocumentColum extends IDocument {
+  key: number
+}
 
 export default function Documents() {
 
+  //Navigate 
+  const navigate = useNavigate()
+
   // Api
   const { data, isLoading } = useGetDocumentQuery();
+
   // Memo
   const filterData = useMemo(() => {
     if (data?.success && data.data) {
-      return data.data;
+      const newData = data.data.map((item) => ({
+        ...item,
+        key: item.id
+      }));
+      return newData
     }
     return [];
   }, [data]);
-  console.log(filterData);
-  
+
+  //handle navigate
+  const toNavigate = (id: number | null) => {
+    if (!id) throw new Error("`document` is required")
+    
+    navigate(main_routes.documentShow.replace(":id", id.toString()));
+  };
+  // columns
+  const columns = useMemo<MRT_ColumnDef<IDocumentColum>[]>(
+    () => [
+      {
+        accessorKey: "id",
+        header: "ID",
+        size: 50,
+      },
+      {
+        accessorKey: "document_name",
+        header: "DOCUMENT TITLE",
+        size: 150,
+      },
+      {
+        accessorKey: "created_at",
+        header: "CREATED DATE",
+        size: 150,
+      },
+      {
+        accessorKey: "configuration_count",
+        header: "DOCUMENT SIZE",
+        size: 150,
+      },
+      {
+        accessorKey: "key",
+        accessorFn: (item) => (
+          <Button onClick={() => toNavigate(item.id)}>Document preview</Button>
+        ),
+        header: "",
+        size: 100,
+      },
+    ],
+    []
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data: filterData,
+    initialState: { density: "compact" },
+    state: {
+      isLoading,
+    },
+  });
   return (
-    <>
-      Document
-    </>
+    <div style={{ width: "100%" }}>
+      <MaterialReactTable table={table} />
+    </div>
   );
 }
